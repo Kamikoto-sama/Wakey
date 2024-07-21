9efab9b7e042c11c3010ddcde7bea4d390b2e606
Dan <38423206+Kamikoto-sama@users.noreply.github.com>
2024-07-21 22:02:05 +0500

    File logging

--- Daemon/Program.cs
+++ Daemon/Program.cs
@@ -1,7 +1,12 @@
-using System.ServiceProcess;
 using Daemon;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+Console.WriteLine(Directory.GetCurrentDirectory());
+builder.Logging.AddFile(
+    @"D:\Projects\WakeOnLan\Wakey\Daemon\bin\Release\publish\logs\log_{Date}",
+    retainedFileCountLimit: 7
+);
 
 builder.Services.AddWindowsService();
 builder.Services.AddSingleton<VpnService>();
