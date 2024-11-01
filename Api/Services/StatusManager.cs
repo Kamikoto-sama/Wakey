dbbee53851921b5e091185c902d7f77059c3d294
Dan <38423206+Kamikoto-sama@users.noreply.github.com>
2024-11-01 21:22:07 +0500

    Log StatusManager.Send

--- Api/Services/StatusManager.cs
+++ Api/Services/StatusManager.cs
@@ -3,14 +3,17 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Api.Services;
 
-public partial class StatusManager(ChargeService chargeService, IHubContext<StatusHub> statusHub)
+public partial class StatusManager(ChargeService chargeService, IHubContext<StatusHub> statusHub, ILogger<StatusManager> log)
 {
     private readonly Status status = new();
 
     public Status GetStatus() => status;
 
-    private void Send(ClientType clientType, string method, bool value) =>
+    private void Send(ClientType clientType, string method, bool value)
+    {
         statusHub.Clients.Group(clientType.ToString()).SendAsync(method, value);
+        log.LogInformation("Called {}({}) of {}", method, value, clientType);
+    }
 }
 
 public record Status
